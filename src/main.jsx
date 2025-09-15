14b6ab4162ea9bc48288316b206f0ddec6798772
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-09-15 12:26:49 +0600

    learnt basic nested routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+import Root from './components/Root/Root/Root.jsx'
+import Home from './components/Root/Home/Home.jsx'
+import Mobiles from './components/Root/Mobiles/Mobiles.jsx'
+import Laptops from './components/Root/Laptops/Laptops.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello from react router</div>
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: 'mobiles', Component: Mobiles },
+      {path: 'laptops', Component: Laptops}
+    ],
   }
 ]);
 
