2f7ff19c767115845d50b539bcca1835a299f6bc
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-09-16 12:36:58 +0600

    adjusted margin in users and posts

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -5,8 +5,8 @@ import './Posts.css'
 const Posts = () => {
     const posts = useLoaderData();
     return (
-        <div>
-            <h2>This is posts</h2>
+        <div style={{marginTop: '20px'}}>
+           
             <div className="posts">
                 {
                     posts.map(post => <Post key={post.id} post={post}></Post>)
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,10 +3,8 @@ import User from "../User/User";
 import './Users.css'
 const Users = () => {
     const users = useLoaderData();
-    console.log(users);
     return (
-        <div>
-            <h2>This is Users</h2>
+        <div style={{marginTop:'20px'}}>
             <div className="users">
                 {
                     users.map(user => <User key={user.id} user={user}></User>)
